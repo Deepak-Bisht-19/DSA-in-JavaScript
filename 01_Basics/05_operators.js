948c737c908df940bd0732706820780553e253e1
deepak bisht <db362966@gmail.com>
2026-07-30 14:18:01 +0530

    pre post

---
+++ 01_Basics/05_operators.js
@@ -0,0 +1,26 @@
+// **********************operators**********************
+
+
+// ******************Arithmetic operators*********************
+// +, -, *, /, %
+
+// we know about +, - , * so let's see the rest
+
+// / operator
+let a = 10;
+let b = 3;
+
+console.log(a / b); // Output: 3.3333333333333335
+// we have to focus that our answer shoud be a integer in js as js can give us decimal values as well so we have to use Math.floor() to get the integer value
+console.log(Math.floor(a / b)); // Output: 3 (floor division)
+
+// % operator
+let c = 9;
+let d = 4;
+
+console.log(c % d); // Output: 1 (remainder)
+
+
+// *******************relational operators*******************
+
+//>, <, =, !=, >=, <=
